6f50a4ca34de1dac357ab1d9da7e281ca2d14e74
Arslan <yamamoto1659@gmail.com>
2018-10-14 12:49:45 +0300

    updates to deploy to heroku 5V

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ app.use(express.static(path.join(__dirname,'dist')));
 app.get('*', (req,res)=> {
   res.sendFile(path.join(__dirname,'dist/index.html'));
 });
-const port = process.env.port || '3001';
+const port = process.env.port || 3000;
 app.set('port', port);
 
 const server = http.createServer(app);
