3267c8cf9792823dba7b8301e1126f5dfffb78a6
Arslan <yamamoto1659@gmail.com>
2018-10-14 12:38:10 +0300

    updates to deploy to heroku 4V

--- server.js
+++ server.js
@@ -1,16 +1,20 @@
 //Install express server
-var express = require('express');
-var path = require('path');
-
-var app = express();
+const express = require('express');
+const http = require('http');
+const path = require('path');
+//const api = require('./server/routes/api');
+const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static(__dirname + '/dist/homefine-frontend'));
+app.use(express.static(path.join(__dirname,'dist')));
 
-app.get('/*', function(req,res) {
-
-  res.sendFile(path.join(__dirname+'/dist/homefine-frontend/index.html'));
+app.get('*', (req,res)=> {
+  res.sendFile(path.join(__dirname,'dist/index.html'));
 });
+const port = process.env.port || '3001';
+app.set('port', port);
+
+const server = http.createServer(app);
 
 // Start the app by listening on the default Heroku port
-app.listen(process.env.PORT || 8080);
+server.listen(port,()=>console.log('Running...'));
